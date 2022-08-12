58cca85d32363702a3c04c07e656dc8a2a8b3217
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-08-12 23:23:32 +0100

    Exercise complete

--- main.py
+++ main.py
@@ -10,8 +10,39 @@ zipped_points = zip(letters, points)
 
 letters_to_points = {key: value for key, value in zip(letters, points)}
 
-print(zipped_points)
-print(letters_to_points)
+letters_to_points[""] = 0
+
+brownie_points = "BROWNIE"
+
+
+def score_word(word):
+    point_total = 0
+    for char in word:
+        point_total += letters_to_points.get(char, 0)
+    return point_total
+
+
+print(score_word(brownie_points))
+
+player_to_words = {
+    "player1": ["BLUE", "TENNIS", "EXIT"],
+    "wordNerd": ["EARTH", "EYES", "MACHINE"],
+    "LexiCon": ["ERASER", "BELLY", "HUSKY"],
+    "ProfReader": ["ZAP", "COMA", "PERIOD"]
+}
+
+player_to_points = {}
+
+for player, words in player_to_words.items():
+
+    player_points = 0
+
+    for w in words:
+        player_points += score_word(w)
+
+    player_to_points[player] = player_points
+
+print(player_to_points)
 
 
 
